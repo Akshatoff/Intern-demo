d647fd6f77b2b28cccd41e562a3678d03ffbef59
Akshatoff <akshatascoder@gmail.com>
2025-05-13 16:36:07 +0530

    second commit error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Home from './components/home'
+import Home from './components/Home'
 import About from './components/About'
 import Services from './components/Services'
 import { ReactLenis, useLenis } from 'lenis/react'
